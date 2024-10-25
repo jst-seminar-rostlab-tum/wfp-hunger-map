78c1f851e6d091aa140328172f405eb4a93d5c2c
Sámuel Fekete <feketesamu@gmail.com>
2024-10-25 22:37:34 +0200

    added highcharts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { Chart } from '@/components/chart.tsx';
 import MapCaller from '@/components/map-caller.tsx';
 
 export default async function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-full">
-      <MapCaller />
+      <section className="h-80 w-full">
+        <MapCaller />
+      </section>
+      <Chart />
     </section>
   );
 }

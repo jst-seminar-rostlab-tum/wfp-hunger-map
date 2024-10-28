c43b43937876ea9148b4c68d7e563a5f1fd551e6
Sámuel Fekete <feketesamu@gmail.com>
2024-10-28 18:30:01 +0100

    added elements page for showcasing

---
+++ src/app/elements/page.tsx
@@ -0,0 +1,9 @@
+import { Chart } from '@/components/Charts/Chart';
+
+/**
+ * You can use this page to try and show off your components.
+ * It's not accessible from the UI, but you can reach it by manually navigating to /elements
+ */
+export default function Elements() {
+  return <Chart />;
+}

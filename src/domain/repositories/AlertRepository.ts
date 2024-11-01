89ff6c3cd0c252afb911f7fc4f9e0a1dc3b6d482
Sámuel Fekete <feketesamu@gmail.com>
2024-11-01 22:11:47 +0100

    docs: basic docs for repository methods

--- src/domain/repositories/AlertRepository.ts
+++ src/domain/repositories/AlertRepository.ts
@@ -2,6 +2,13 @@ import { Conflict } from '../entities/alerts/Conflict';
 import { Hazard } from '../entities/alerts/Hazard';
 
 export interface AlertRepository {
+  /**
+   * Returns all the hazards (flooding, forest fire, etc.) in the world
+   */
   getHazardData(): Promise<Hazard[]>;
+
+  /**
+   * Returns all the conflicts (battles, riots, etc.) in the world
+   */
   getConflictData(): Promise<Conflict[]>;
 }
--- src/domain/repositories/GlobalDataRepository.ts
+++ src/domain/repositories/GlobalDataRepository.ts
@@ -5,9 +5,28 @@ import { DisputedAreas } from '../entities/DisputedAreas';
 import { YearInReview } from '../entities/YearInReview';
 
 export interface GlobalDataRepository {
+  /**
+   * Returns the national IPC data of all the countries
+   */
   getIpcData(): Promise<CountryIpcData[]>;
+
+  /**
+   * Not sure what this returns :/
+   */
   getAdm0Data(): Promise<Adm0Data[]>;
+
+  /**
+   * Returns all the disputed areas around the world
+   */
   getDisputedAreas(): Promise<DisputedAreas>;
+
+  /**
+   * Returns the change log of the WFP HungerMap
+   */
   getChangeLog(): Promise<ChangeLogItem[]>;
+
+  /**
+   * Return the Year in Review reports of the WFP HungerMap
+   */
   getYearInReviews(): Promise<YearInReview[]>;
 }

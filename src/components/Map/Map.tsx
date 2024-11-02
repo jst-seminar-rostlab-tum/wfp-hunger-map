9e0dcb3bac136d7102d6eace21df00153c944381
georgi4444 <georgi.tsipov@gmail.com>
2024-11-03 00:16:44 +0100

    feat: inital sidebar implementation

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -6,7 +6,7 @@ import { MapContainer, TileLayer } from 'react-leaflet';
 
 export default function Map() {
   return (
-    <MapContainer center={[21.505, -0.09]} zoom={4} style={{ height: '100%', width: '100%' }}>
+    <MapContainer center={[21.505, -0.09]} zoom={4} style={{ height: '100%', width: '100%', zIndex: 40 }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"

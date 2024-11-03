857f7d02fbb71bceaa775bed2a6ba501e1dc1c4b
plaume8 <linus.sander@icloud.com>
2024-11-03 21:18:01 +0100

    feat: setup sandbox

--- src/app/sandbox/page.tsx
+++ src/app/sandbox/page.tsx
@@ -1,3 +1,35 @@
+'use client';
+
+import { Switch } from '@nextui-org/switch';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+
 export default function Sandbox() {
-  return <div>Sandbox</div>;
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <>
+      <div className="w-full h-10 p-4 flex flex-row items-center gap-4 border-b border-gray-800">
+        <p> Change dark/light mode: </p>
+        <Switch
+          onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          aria-label="Toggle theme"
+          size="sm"
+        />
+      </div>
+      <div className="w-full h-fit min-h-60 p-4 border-b border-gray-800">
+        Sandbox 1
+      </div>
+      <div className="w-full h-fit min-h-60 p-4 border-b border-gray-800">
+        Sandbox 2
+      </div>
+    </>
+  );
 }

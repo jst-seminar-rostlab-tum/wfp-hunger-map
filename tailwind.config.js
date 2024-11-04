1411aaa52a2e26fa8cf6a694bc410f965c091711
Sámuel Fekete <feketesamu@gmail.com>
2024-11-04 23:36:44 +0100

    fix: use correct paths in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,8 @@ import { nextui } from '@nextui-org/theme';
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
     './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
@@ -12,6 +12,10 @@ module.exports = {
       fontFamily: {
         sans: ['var(--font-sans)'],
         mono: ['var(--font-mono)'],
+      },
+      colors: {
+        brand: '#157DBC',
+        brandHover: '#0F6396',
       },
     },
   },

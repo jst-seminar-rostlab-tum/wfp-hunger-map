8da306277129f9e7583a1f2dfc65a60cc9b3b7ff
georgi4444 <georgi.tsipov@gmail.com>
2024-11-11 13:34:43 +0100

    fix: replace Listbox with buttons in Sidebar components

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ import { Button } from '@nextui-org/button';
 import { Card, CardBody, CardFooter, CardHeader } from '@nextui-org/card';
 import { Input } from '@nextui-org/input';
 import { Link } from '@nextui-org/link';
-import { Listbox, ListboxItem, ListboxSection } from '@nextui-org/listbox';
+import clsx from 'clsx';
 import { SidebarLeft } from 'iconsax-react';
 import NextImage from 'next/image';
 
@@ -13,7 +13,6 @@ import { CollapsedSidebar } from '@/components/Sidebar/CollapsedSidebar';
 import { ThemeSwitch } from '@/components/Sidebar/ThemeSwitch';
 import { useSidebar } from '@/domain/contexts/SidebarContext';
 import { AlertsMenuVariant } from '@/domain/enums/AlertsMenuVariant';
-import { GlobalInsight } from '@/domain/enums/GlobalInsight';
 import { SidebarOperations } from '@/operations/sidebar/SidebarOperations';
 
 const pagesLinks = [
@@ -27,20 +26,8 @@ const pagesLinks = [
 export function Sidebar() {
   const { isSidebarOpen, toggleSidebar, selectedMapType, setSelectedMapType } = useSidebar();
 
-  const handleSelectionChange = (key: 'all' | Set<string | number>) => {
-    return key instanceof Set
-      ? setSelectedMapType(Array.from(key)[0] as GlobalInsight)
-      : setSelectedMapType(key as GlobalInsight);
-  };
-
   if (!isSidebarOpen) {
-    return (
-      <CollapsedSidebar
-        selectedMapType={selectedMapType}
-        handleSelectionChange={handleSelectionChange}
-        toggleSidebar={toggleSidebar}
-      />
-    );
+    return <CollapsedSidebar />;
   }
 
   return (
@@ -59,33 +46,28 @@ export function Sidebar() {
               <SidebarLeft size={24} />
             </Button>
           </div>
-
           <ThemeSwitch />
           <Input className="w-full" color="primary" placeholder="Search a country" variant="faded" />
         </CardHeader>
         <CardBody>
-          <Listbox
-            variant="flat"
-            aria-label="Listbox menu with sections"
-            selectionMode="single"
-            selectedKeys={new Set(selectedMapType)}
-            onSelectionChange={handleSelectionChange}
-            disallowEmptySelection
-            hideSelectedIcon
-          >
-            <ListboxSection title="Global Insights">
+          <div className="p-1 w-full">
+            <span className="text-tiny text-foreground-500 pl-1">Global Insights</span>
+            <div className="flex flex-col gap-1 pt-1">
               {SidebarOperations.getSidebarMapTypes().map((item) => (
-                <ListboxItem
-                  key={item.key}
+                <Button
                   startContent={item.icon && <NextImage src={item.icon} alt={item.label} width={24} height={24} />}
+                  key={item.key}
+                  variant={selectedMapType === item.key ? undefined : 'light'}
+                  className={clsx('justify-start', selectedMapType === item.key && 'bg-primary')}
+                  onClick={() => setSelectedMapType(item.key)}
                 >
                   {item.label}
-                </ListboxItem>
+                </Button>
               ))}
-            </ListboxSection>
-          </Listbox>
+            </div>
+          </div>
           <div className="p-1 w-full">
-            <span className="text-xs text-foreground-500 pl-1">Alerts</span>
+            <span className="text-tiny text-foreground-500 pl-1">Alerts</span>
             <div className="pt-1">
               <AlertsMenu variant={AlertsMenuVariant.Inside} />
             </div>

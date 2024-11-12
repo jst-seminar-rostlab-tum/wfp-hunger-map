dc92f3c954134f43f567d772e9b6ff6f909421c8
georgi4444 <georgi.tsipov@gmail.com>
2024-11-12 21:23:44 +0100

    feat: Wrap logo component with a Link to homepage

--- src/components/LogoWithText/LogoWithText.tsx
+++ src/components/LogoWithText/LogoWithText.tsx
@@ -1,12 +1,15 @@
 import NextImage from 'next/image';
+import NextLink from 'next/link';
 
 export function LogoWithText() {
   return (
-    <div className="flex items-center gap-2">
-      <NextImage src="/wfp_logo.svg" alt="HungerMap" width={45} height={45} />
-      <p className="font-bold text-lg">
-        HungerMap <sup>LIVE</sup>
-      </p>
-    </div>
+    <NextLink href="/">
+      <div className="flex items-center gap-2">
+        <NextImage src="/wfp_logo.svg" alt="HungerMap" width={45} height={45} />
+        <p className="font-bold text-lg">
+          HungerMap <sup>LIVE</sup>
+        </p>
+      </div>
+    </NextLink>
   );
 }

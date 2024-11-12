fd71c58f930305f8e9aca0d10358fb840bd9891d
Haidong Xu <hai_designer@hotmail.com>
2024-11-12 20:11:08 +0100

    fix: icon, scrollbar, background and interface style

--- src/components/Chatbot/Chatbot.tsx
+++ src/components/Chatbot/Chatbot.tsx
@@ -2,10 +2,10 @@
 
 'use client';
 
-import { Button, Card, CardBody, CardFooter, CardHeader, Divider, Tooltip } from '@nextui-org/react';
+import { Button, Card, CardBody, CardFooter, CardHeader, Divider, ScrollShadow, Tooltip } from '@nextui-org/react';
 import clsx from 'clsx';
-import { CloseCircle, Send2, SidebarLeft, SidebarRight } from 'iconsax-react';
-import { Bot, Maximize2, Minimize2 } from 'lucide-react';
+import { CloseSquare, Maximize4, Minus, Send2, SidebarLeft, SidebarRight } from 'iconsax-react';
+import { Bot } from 'lucide-react';
 import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 
@@ -182,9 +182,9 @@ export default function HungerMapChatbot() {
         <Button
           onClick={toggleChat}
           className="
-            relative flex items-center justify-center min-w-12 h-12 px-1 rounded-full border-2 border-black dark:border-white bg-white dark:bg-transparent"
+            relative flex items-center justify-center min-w-12 h-12 px-1 rounded-full bg-white dark:bg-black shadow-md"
         >
-          <Bot className="stroke-black dark:stroke-white w-6 h-6" />
+          <Bot size={24} />
         </Button>
       )}
       {/* chatbot interface */}
@@ -201,17 +201,15 @@ export default function HungerMapChatbot() {
           )}
           <Card
             className={clsx(
-              isFullScreen
-                ? 'w-screen h-screen rounded-none opacity-100 border-0'
-                : 'w-[636px] h-[657px] opacity-80 border-1',
+              isFullScreen ? 'w-screen h-screen rounded-none' : 'w-[636px] h-[657px]',
               isSidebarOpen ? (isFullScreen ? 'sm:pl-[215px] pl-0' : 'sm:pl-[179px] pl-0') : 'pl-0',
-              'border-solid border-black bg-white dark:bg-black overflow-hidden flex-1 flex flex-col text-black dark:text-white'
+              'overflow-hidden flex-1 flex flex-col text-black dark:text-white'
             )}
           >
             <CardHeader className="flex items-center justify-between p-4">
               <div className="flex items-center space-x-2">
                 <Button variant="light" isIconOnly onClick={() => setIsSidebarOpen((previousValue) => !previousValue)}>
-                  {isSidebarOpen ? <SidebarLeft className="h-4 w-4" /> : <SidebarRight className="h-4 w-4" />}
+                  {isSidebarOpen ? <SidebarLeft size={24} /> : <SidebarRight size={24} />}
                 </Button>
                 <Image src="/wfp-logo.png" width={32} height={32} alt="WFP Logo" className="mr-2" />
                 <h2 className="text-lg font-semibold truncate">{CHAT_TITLE}</h2>
@@ -220,109 +218,104 @@ export default function HungerMapChatbot() {
                 {!isMobile && (
                   <Tooltip content={isFullScreen ? EXIT_FULL_SCREEN : ENTER_FULL_SCREEN}>
                     <Button variant="light" isIconOnly onClick={toggleFullScreen}>
-                      {isFullScreen ? <Minimize2 className="h-4 w-4" /> : <Maximize2 className="h-4 w-4" />}
+                      {isFullScreen ? <Minus size={24} /> : <Maximize4 size={24} />}
                     </Button>
                   </Tooltip>
                 )}
                 <Tooltip content="Close Chat">
                   <Button variant="light" isIconOnly onClick={toggleChat}>
-                    <CloseCircle className="h-4 w-4" />
+                    <CloseSquare size={24} />
                   </Button>
                 </Tooltip>
               </div>
             </CardHeader>
             <Divider className="bg-chatbotDivider dark:bg-chatbotDivider" />
-            <CardBody className="p-0 h-full">
-              <div className="relative h-full flex flex-col">
-                {/* overlay area in mobile version */}
-                {isMobile && isSidebarOpen && (
-                  /* since it has been show as overlay style here, once click this area then close side panel better not use button here */
-                  // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
-                  <div
-                    className="fixed inset-0 z-10 opacity-50 bg-white dark:bg-black"
-                    onClick={() => setIsSidebarOpen((previousValue) => !previousValue)}
-                  />
-                )}
-                {/* chat area */}
+            <CardBody>
+              {/* overlay area in mobile version */}
+              {isMobile && isSidebarOpen && (
+                /* since it has been show as overlay style here, once click this area then close side panel better not use button here */
+                // eslint-disable-next-line jsx-a11y/click-events-have-key-events, jsx-a11y/no-static-element-interactions
+                <div
+                  className="fixed inset-0 z-10 opacity-50 bg-white dark:bg-black"
+                  onClick={() => setIsSidebarOpen((previousValue) => !previousValue)}
+                />
+              )}
+              {/* chat area */}
 
-                <div className="flex-1 p-4 overflow-y-auto">
-                  {chats[currentChatIndex].messages.length === 0 ? (
-                    <div className="flex flex-col items-center mt-4 h-full space-y-4">
-                      <p className="text-center text-xl max-w-[80%] mb-2">{WELCOME_MESSAGE}</p>
-                      <p className="text-center text-md max-w-[80%] mb-2">{SUB_WELCOME_MESSAGE}</p>
-                      <div className="flex flex-col items-center space-y-2 w-full max-w-md">
-                        {DEFAULT_PROMPT.map((prompt) => (
-                          <Button
-                            key={prompt.id}
-                            onClick={(event) => handleSubmit(event, prompt.value)}
-                            className="
-                              truncate w-full mb-2 max-w-[250px] sm:max-w-[400px] border border-solid border-black dark:border-white bg-transparent hover:bg-chatbotDefaultPromptHover dark:hover:bg-chatbotDefaultPromptHover opacity-100 dark:hover:opacity-60
+              <ScrollShadow hideScrollBar className="w-full h-full">
+                {chats[currentChatIndex].messages.length === 0 ? (
+                  <div className="flex flex-col items-center mt-4 h-full space-y-4">
+                    <p className="text-center text-xl max-w-[80%] mb-2">{WELCOME_MESSAGE}</p>
+                    <p className="text-center text-md max-w-[80%] mb-2">{SUB_WELCOME_MESSAGE}</p>
+                    <div className="flex flex-col items-center space-y-2 w-full max-w-md">
+                      {DEFAULT_PROMPT.map((prompt) => (
+                        <Button
+                          key={prompt.id}
+                          onClick={(event) => handleSubmit(event, prompt.value)}
+                          className="
+                              truncate w-full mb-2 max-w-[250px] sm:max-w-[400px] border border-solid border-black dark:border-white bg-transparent hover:bg-chatbotDefaultPromptHover dark:hover:bg-chatbotDefaultPromptHover
                               "
-                            title={prompt.value}
-                          >
-                            <span className="truncate">{prompt.value}</span>
-                          </Button>
-                        ))}
-                      </div>
+                          title={prompt.value}
+                        >
+                          <span className="truncate">{prompt.value}</span>
+                        </Button>
+                      ))}
                     </div>
-                  ) : (
-                    chats[currentChatIndex].messages.map((message) => (
+                  </div>
+                ) : (
+                  chats[currentChatIndex].messages.map((message) => (
+                    <div
+                      key={message.id}
+                      className={clsx('flex mb-4', message.role === SenderRole.USER ? 'justify-end' : 'justify-start')}
+                    >
+                      {message.role === SenderRole.ASSISTANT && (
+                        <div className="relative flex items-center justify-center bg-transparent w-10 h-10 rounded-full border-2 border-black dark:border-white bg-white dark:bg-black">
+                          <Bot size={24} />
+                        </div>
+                      )}
                       <div
-                        key={message.id}
                         className={clsx(
-                          'flex mb-4',
-                          message.role === SenderRole.USER ? 'justify-end' : 'justify-start'
+                          'p-2 mb-5 rounded-lg max-w-[80%]',
+                          message.role === SenderRole.USER
+                            ? 'rounded-xl bg-chatbotUserMsg dark:bg-chatbotUserMsg ml-12'
+                            : 'bg-transparent'
                         )}
                       >
-                        {message.role === SenderRole.ASSISTANT && (
-                          <div className="relative flex items-center justify-center bg-transparent w-10 h-10 rounded-full border-2 border-black dark:border-white bg-white dark:bg-black">
-                            <Bot className="w-6 h-6 stroke-black dark:stroke-white" />
+                        {message.role === SenderRole.USER ? (
+                          <p className="break-words text-justify">{message.content}</p>
+                        ) : (
+                          <TypingText
+                            text={message.content}
+                            speed={100}
+                            endSentencePause={500}
+                            onTypingComplete={() => setIsTyping(false)}
+                          />
+                        )}
+                        {message.dataSources && (
+                          <div className="mt-2 text-xs text-gray-600 dark:text-gray-400">
+                            <p className="truncate">{DATA_SOURCES}</p>
+                            <ul className="list-disc pl-4">
+                              {message.dataSources.map((source) => (
+                                <li key={source}>{source}</li>
+                              ))}
+                            </ul>
                           </div>
                         )}
-                        <div
-                          className={clsx(
-                            'p-4 mb-5 rounded-lg max-w-[80%]',
-                            message.role === SenderRole.USER
-                              ? 'rounded-xl bg-chatbotUserMsg dark:bg-chatbotUserMsg ml-12'
-                              : 'bg-transparent'
-                          )}
-                        >
-                          {message.role === SenderRole.USER ? (
-                            <p className="break-words text-justify">{message.content}</p>
-                          ) : (
-                            <TypingText
-                              text={message.content}
-                              speed={100}
-                              endSentencePause={500}
-                              onTypingComplete={() => setIsTyping(false)}
-                            />
-                          )}
-                          {message.dataSources && (
-                            <div className="mt-2 text-xs text-gray-600 dark:text-gray-400">
-                              <p className="truncate">{DATA_SOURCES}</p>
-                              <ul className="list-disc pl-4">
-                                {message.dataSources.map((source) => (
-                                  <li key={source}>{source}</li>
-                                ))}
-                              </ul>
-                            </div>
-                          )}
-                        </div>
                       </div>
-                    ))
-                  )}
-
-                  {isTyping && (
-                    <div className="flex justify-start mb-4">
-                      <div className="relative flex items-center justify-center bg-transparent w-12 h-12 rounded-full border-2 border-black dark:border-white bg-white dark:bg-black">
-                        <Bot className="w-6 h-6 stroke-black dark:stroke-white" />
-                      </div>
-                      <TypingDots />
                     </div>
-                  )}
-                  <div ref={chatEndRef} />
-                </div>
-              </div>
+                  ))
+                )}
+
+                {isTyping && (
+                  <div className="flex justify-start mb-4">
+                    <div className="relative flex items-center justify-center bg-transparent w-10 h-10 rounded-full border-2 border-black dark:border-white bg-white dark:bg-black">
+                      <Bot />
+                    </div>
+                    <TypingDots />
+                  </div>
+                )}
+                <div ref={chatEndRef} />
+              </ScrollShadow>
             </CardBody>
             <Divider className="bg-chatbotDivider dark:bg-chatbotDivider" />
             <CardFooter className="p-4">
@@ -334,12 +327,14 @@ export default function HungerMapChatbot() {
                     onChange={(inputEvent) => setInput(inputEvent.target.value)}
                     onKeyDown={handleKeyDown}
                     placeholder={TYPING_PLACEHOLDER}
-                    className="rounded-xl border border-solid border-black bg-chatbotInputArea dark:bg-chatbotInputArea flex-grow px-3 py-2 mr-2 dark:text-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none overflow-hidden"
+                    className="rounded-xl border border-solid border-black bg-chatbotInputArea dark:bg-chatbotInputArea flex-grow px-3 py-2 dark:text-white text-gray-800 focus:outline-none focus:ring-2 focus:ring-blue-500 resize-none overflow-hidden"
                     rows={1}
                   />
+                  {/*                     className="rounded-xl border-2 border-solid border-black dark:border-white hover:border-blue-500 focus:border-blue-500 dark:hover:border-blue-500 dark:focus:border-blue-500 flex-grow px-3 py-2 dark:text-white text-gray-800 resize-none overflow-hidden"
+                   */}
                   <Tooltip content="Submit">
                     <Button type="submit" variant="light" isIconOnly disabled={isTyping}>
-                      <Send2 className="h-4 w-4" />
+                      <Send2 size={24} />
                     </Button>
                   </Tooltip>
                 </div>
--- src/components/Chatbot/ChatbotSidebar.tsx
+++ src/components/Chatbot/ChatbotSidebar.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable no-nested-ternary */
 import { Button, Card, CardBody } from '@nextui-org/react';
 import clsx from 'clsx';
-import { PlusCircle } from 'lucide-react';
+import { AddCircle } from 'iconsax-react';
 
 import { NEW_CHAT_BUTTON } from '@/domain/constant/chatbot/Chatbot';
 import { ChatbotSidebarProps } from '@/domain/props/ChatbotSidebarProps';
@@ -25,9 +25,9 @@ export default function ChatbotSidebar({
       <CardBody className="p-4">
         <Button
           onClick={onStartNewChat}
-          className="bg-transparent w-full h-10 mb-4 flex justify-center items-center gap-2 border-2 border-solid border-black dark:border-white text-black dark:text-white hover:bg-chatbotSidebarBtnHover dark:hover:bg-chatbotSidebarBtnHover"
+          className="bg-transparent w-full h-10 mb-4 flex justify-center items-center gap-2 border-1.5 border-solid border-black dark:border-white text-black dark:text-white hover:bg-chatbotSidebarBtnHover dark:hover:bg-chatbotSidebarBtnHover"
         >
-          <PlusCircle className="h-4 w-4 text-black dark:text-white" />
+          <AddCircle size={24} />
           <span className="truncate text-black dark:text-white">{NEW_CHAT_BUTTON}</span>
         </Button>
         <div className="h-[calc(100%-60px)] overflow-y-auto">

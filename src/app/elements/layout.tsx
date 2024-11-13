773ce7ba076f3a7ba290710327b6ae74bb8fe395
georgi4444 <georgi.tsipov@gmail.com>
2024-11-13 09:47:35 +0100

    feat: add metadata for About and Elements pages

--- src/app/elements/layout.tsx
+++ src/app/elements/layout.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from 'next';
+
 import { Topbar } from '@/components/Topbar/Topbar';
+
+export const metadata: Metadata = {
+  title: 'Elements',
+};
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (

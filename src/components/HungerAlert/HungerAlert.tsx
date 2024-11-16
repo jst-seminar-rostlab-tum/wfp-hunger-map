cf098d0a1e30c08dd4a5e5ddddaaf9a39710c5c6
Armanpreet Ghotra <arman.ghotra@tum.de>
2024-11-16 09:52:46 +0100

    fix: reduce the size of button, remove the text and add tooltip

--- src/components/HungerAlert/HungerAlert.tsx
+++ src/components/HungerAlert/HungerAlert.tsx
@@ -6,6 +6,7 @@ import { useMemo, useState } from 'react';
 
 import DataTable from '@/components/DataTable/DataTable';
 import PopupModal from '@/components/PopupModal/PopupModal';
+import { Tooltip } from '@/components/Tooltip/Tooltip';
 import HungerLevel from '@/domain/entities/HungerLevel';
 import HungerAlertProps from '@/domain/props/HungerAlertProps';
 
@@ -22,12 +23,16 @@ export default function HungerAlert({ countryMapData }: HungerAlertProps) {
   );
 
   return (
-    <div className="absolute bottom-20 left-20 z-10 cursor-pointer">
-      <button className={HungerAlertOperations.getPulseClasses()} onClick={toggleModal} type="button">
-        <p className="text-4xl font-bold mb-2">{countriesWithHighHunger.length}</p>
-        <p className="text-center font-medium text-sm px-4">Number of Countries with Very High Levels of Hunger</p>
-      </button>
-
+    <div className="absolute bottom-0 left-0 z-10 cursor-pointer" style={{ bottom: '12rem', left: '1.2rem' }}>
+      <Tooltip text="Number of countries with high hunger levels" warning>
+        <button
+          className={`${HungerAlertOperations.getPulseClasses()} p-2 text-sm`}
+          onClick={toggleModal}
+          type="button"
+        >
+          <p className="text-4xl">{countriesWithHighHunger.length}</p>
+        </button>
+      </Tooltip>
       <PopupModal
         isModalOpen={isModalOpen}
         toggleModal={toggleModal}

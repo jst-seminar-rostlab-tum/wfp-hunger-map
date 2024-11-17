d75d0f0c4dbe1f5cca027d0c04412362048a2b32
Haidong Xu <haidong.xu@tum.de>
2024-11-17 16:21:16 +0100

    Feature/f 61 finish chatbot (#30)
    * fix: icon, scrollbar, background and interface style
    
    * feat: add tooltip for trigger icon
    
    * fix: scrollbar area
    
    * fix: when typing bot should not have waiting response animation
    
    ---------
    
    Co-authored-by: Haidong Xu <hai_designer@hotmail.com>

--- src/components/TypingText/TypingDot.tsx
+++ src/components/TypingText/TypingDot.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function TypingDots() {
   return (
-    <div className="flex mt-5 ml-2 space-x-2">
+    <div className="flex mt-4 ml-2 space-x-2">
       <span className="inline-block w-2 h-2 bg-current rounded-full animate-pulse dot-delay-1" />
       <span className="inline-block w-2 h-2 bg-current rounded-full animate-pulse dot-delay-2" />
       <span className="inline-block w-2 h-2 bg-current rounded-full animate-pulse dot-delay-3" />

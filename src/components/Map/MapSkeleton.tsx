3df5d24ceb9d72f5980858cd97907a4366f9f980
Lukas0912 <lukas_weigmann@web.de>
2024-11-19 19:00:32 +0100

    feat: skeleton of map while loading

--- src/components/Map/MapSkeleton.tsx
+++ src/components/Map/MapSkeleton.tsx
@@ -1,13 +1,5 @@
-import { Skeleton } from '@nextui-org/skeleton';
+import MapSkeletonData from '@/domain/constant/map/MapSkeletonData';
 
 export default function MapSkeleton() {
-  return (
-    <div className="w-screen h-screen bg-blue-100">
-      <div className="w-full h-full p-4 flex flex-col items-center justify-center">
-        <div className="relative w-64 h-64 md:w-96 md:h-96 rounded-full bg-blue-500 overflow-hidden">
-          <Skeleton className="absolute w-full h-full rounded-full animate-pulse" />
-        </div>
-      </div>
-    </div>
-  );
+  return <MapSkeletonData className="w-full h-full animate-opacityPulse fill-countriesBase bg-ocean" />;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -76,6 +76,7 @@ const config = {
       animation: {
         blink: 'blink 1s step-end infinite',
         pulse: 'pulse 1.5s ease-in-out infinite',
+        opacityPulse: 'opacityPulse 1.5s infinite',
       },
       keyframes: {
         blink: {
@@ -84,6 +85,10 @@ const config = {
         pulse: {
           '0%, 100%': { transform: 'scale(1)', opacity: '0.6' },
           '50%': { transform: 'scale(1.3)', opacity: '1' },
+        },
+        opacityPulse: {
+          '0%, 100%': { 'fill-opacity': '1' },
+          '50%': { 'fill-opacity': '0.5' },
         },
       },
     },
@@ -117,8 +122,6 @@ const config = {
             chatbotUserMsg: '#E6F1FE',
             chatbotDivider: '#292d32',
             surfaceGrey: '#B0B0B0',
-            activeCountries: '#82bce0',
-            inactiveCountries: '#a7b3ba',
             subscribeText: '#284366',
             conflictProtest: '#0d657de6',
             conflictRiot: '#c95200e6',
@@ -140,6 +143,8 @@ const config = {
             vegetationLow: '#b99260',
             vegetationNormal: '#fff',
             vegetationHigh: '#b1dbb5',
+            countriesBase: '#fefeff',
+            ocean: '#91cccb'
           },
         },
         dark: {
@@ -167,8 +172,6 @@ const config = {
             chatbotUserMsg: '#26262A',
             chatbotDivider: '#556372',
             surfaceGrey: '#444444',
-            activeCountries: '#115884',
-            inactiveCountries: '#85929b',
             subscribeText: '#ffffff',
             conflictProtest: '#0d657de6',
             conflictRiot: '#c95200e6',
@@ -176,6 +179,8 @@ const config = {
             conflictCivil: '#96badc',
             conflictExplosion: '#eaaf75',
             conflictStrategic: '#bec0c1',
+            countriesBase: '#0e6397',
+            ocean: '#111111'
           },
         },
       },

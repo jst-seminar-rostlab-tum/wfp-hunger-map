29e3e3f1cbba6be19dc91e65f4ac725534ade8f4
georgi4444 <georgi.tsipov@gmail.com>
2024-11-20 17:21:58 +0100

    fix: correct greater sign

--- src/domain/constant/alerts/CountryAlertsConstants.ts
+++ src/domain/constant/alerts/CountryAlertsConstants.ts
@@ -8,6 +8,6 @@ export const countryAlertsColors: Record<CountryAlertType, string> = {
 
 export const countryAlertsLabels: Record<CountryAlertType, string> = {
   [CountryAlertType.FATALITY]: 'Countries with ≥ 1 fatality /200,000 in the last 30 days.',
-  [CountryAlertType.CLIMATE_WET]: '% of people living in areas with significant excess rainfall &gt; 15%',
-  [CountryAlertType.CLIMATE_DRY]: '% of people living in areas with deficit in rainfall or vegetation &gt; 15%',
+  [CountryAlertType.CLIMATE_WET]: '% of people living in areas with significant excess rainfall > 15%',
+  [CountryAlertType.CLIMATE_DRY]: '% of people living in areas with deficit in rainfall or vegetation > 15%',
 };

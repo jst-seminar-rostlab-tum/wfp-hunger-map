523297aaba39f641d08d6175dc3413ca92ead794
Haidong Xu <hai_designer@hotmail.com>
2024-11-23 17:48:13 +0100

    feat: pop up legend content

--- src/components/Legend/LegendContainer.tsx
+++ src/components/Legend/LegendContainer.tsx
@@ -1,13 +1,14 @@
 import { Button } from '@nextui-org/button';
-import { Add } from 'iconsax-react';
+import { Information } from 'iconsax-react';
 import { useState } from 'react';
 
-import { TRIGGER_CHAT } from '@/domain/constant/chatbot/Chatbot';
+import { INFO_POPUP_TOOLTIP } from '@/domain/constant/legend/mapLegendData';
 import LegendContainerProps from '@/domain/props/LegendContainerProps';
 import { LegendOperations } from '@/operations/legends/LegendOperations.ts';
 import { useMediaQuery } from '@/utils/resolution';
 
 import CustomAccordion from '../Accordions/Accordion';
+import PopupModal from '../PopupModal/PopupModal';
 import { Tooltip } from '../Tooltip/Tooltip';
 import GradientLegend from './GradientLegend';
 import PointLegend from './PointLegend';
@@ -17,7 +18,7 @@ export default function LegendContainer({ items, loading = false }: LegendContai
   const [showInfoPopup, setInfoPopup] = useState(false);
 
   return !isMobile ? (
-    <div className="w-[450px]">
+    <div className="w-[450px] absolute bottom-5 right-0 z-50 mr-10">
       <CustomAccordion
         loading={loading}
         items={items.map((item) => ({
@@ -33,16 +34,47 @@ export default function LegendContainer({ items, loading = false }: LegendContai
       />
     </div>
   ) : (
-    <div className="absolute z-[9999] top-4 right-4">
-      <Tooltip text={TRIGGER_CHAT}>
-        <Button
-          onClick={() => setInfoPopup(!showInfoPopup)}
-          className="
+    <>
+      <div className="absolute z-50 bottom-3 right-16">
+        <Tooltip text={INFO_POPUP_TOOLTIP}>
+          <Button
+            onClick={() => setInfoPopup(true)}
+            className="
         relative flex items-center justify-center min-w-12 h-12 px-1 rounded-full bg-content1 shadow-md"
+          >
+            <Information size={32} />
+          </Button>
+        </Tooltip>
+      </div>
+      {showInfoPopup && (
+        <PopupModal
+          isModalOpen={showInfoPopup}
+          toggleModal={() => setInfoPopup(!showInfoPopup)}
+          modalTitle=""
+          modalSize="lg"
+          modalHeight="auto"
         >
-          <Add size={24} />
-        </Button>
-      </Tooltip>
-    </div>
+          {items.map((item) =>
+            LegendOperations.isGradientLegendContainerItem(item) ? (
+              <>
+                <div key={item.title} className="font-bold">
+                  {item.title}
+                </div>
+                <GradientLegend key={item.title} {...item} />
+              </>
+            ) : (
+              <>
+                <div key={item.title} className="font-bold">
+                  {item.title}
+                </div>
+                <PointLegend key={item.title} {...item}>
+                  {item.renderItem ? (props) => item.renderItem?.(props) : undefined}
+                </PointLegend>
+              </>
+            )
+          )}
+        </PopupModal>
+      )}
+    </>
   );
 }

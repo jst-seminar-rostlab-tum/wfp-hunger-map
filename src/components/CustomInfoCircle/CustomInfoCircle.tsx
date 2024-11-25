7d98b55e2190a0e43bf5563c140f9993307dad4b
Jakob Schödl <jakob.schoedl@tum.de>
2024-11-25 15:47:58 +0100

    fix: info icon alignment on /data_sources

--- src/components/CustomInfoCircle/CustomInfoCircle.tsx
+++ src/components/CustomInfoCircle/CustomInfoCircle.tsx
@@ -2,21 +2,11 @@ import { InfoCircle } from 'iconsax-react';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-function CustomInfoCircle({ style, ...props }: React.ComponentProps<typeof InfoCircle>) {
-  return (
-    <InfoCircle
-      {...props}
-      style={{
-        transform: 'rotate(180deg)',
-        ...style,
-      }}
-    />
-  );
+function CustomInfoCircle({ className, ...props }: React.ComponentProps<typeof InfoCircle>) {
+  return <InfoCircle {...props} className={`rotate-180 ${className}`} />;
 }
 CustomInfoCircle.propTypes = {
-  style: PropTypes.shape({
-    transform: PropTypes.string,
-  }),
+  className: PropTypes.string,
 };
 
 export default CustomInfoCircle;
--- src/components/Tooltip/Abbreviation.tsx
+++ src/components/Tooltip/Abbreviation.tsx
@@ -14,9 +14,7 @@ function Abbreviation({ abbreviation }: { abbreviation: keyof typeof abbreviatio
     <Tooltip text={abbreviations[abbreviation]}>
       <div className="inline cursor-help whitespace-nowrap">
         <span className="underline decoration-dotted decoration-2">{abbreviation}</span>
-        <span className="w-3 h-6 inline pb-3">
-          <CustomInfoCircle />
-        </span>
+        <CustomInfoCircle className="w-4 h-6 inline pt-2" />
       </div>
     </Tooltip>
   );

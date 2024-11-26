b8fa5be1b4121b7c071ae458e48c1dc1dfe0bbde
Jakob Schödl <jakob.schoedl@tum.de>
2024-11-26 15:39:52 +0100

    feat: add /disclaimer page

---
+++ src/app/disclaimer/page.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Page() {
+  return (
+    <div className="text-content">
+      <h1>Disclaimer</h1>
+      <p>
+        The designations employed and the presentation of material for all the maps on this site{' '}
+        <b>do not imply the expression of any opinion whatsoever</b> on the part of the World Food Programme concerning
+        the legal or constitutional status of any country, territory, city or area or of its authorities, or concerning
+        the delimitation of its frontiers or boundaries.
+      </p>
+      <ul>
+        <li>
+          Dotted line represents approximately the Line of Control in Jammu and Kashmir agreed upon by India and
+          Pakistan. The final status of Jammu and Kashmir has not yet been agreed upon by the parties.
+        </li>
+        <li>
+          Final boundary between the Republic of Sudan and the Republic of South Sudan has not yet been determined.
+        </li>
+        <li>Final status of the Abyei area is not yet determined.</li>
+        <li>
+          A dispute exists between the Governments of Argentina and the United Kingdom of Great Britain and Northern
+          Ireland concerning sovereignty over the Falkland Islands (Malvinas).
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default Page;

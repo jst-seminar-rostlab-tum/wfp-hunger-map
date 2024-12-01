e489a424666982c40fbec84278d3f6a6664a75bd
Haidong Xu <haidong.xu@tum.de>
2024-12-01 12:09:48 +0100

    Feature/f 109 subscribe api connection concrete topics (#75)
    * feat: init get method and change data structure
    
    * feat: change interface
    
    * feat: init nested popover
    
    * fix: popover menu style
    
    * feat: multiple selection of options
    
    * feat: handle callback logic in nested popover
    
    * fix: improve callback logic
    
    * feat: change label when select option
    
    * feat: concise prop and label
    
    * fix: temp delete
    
    * fix: enclose submit button componenet
    
    * fix: yarn lint
    
    * fix: use backend topic endpoint
    
    * fix: api call
    
    * fix: api call
    
    * fix: api and data structure and add tooltip for select options
    
    * fix: api call and data structure
    
    * fix: make org and topic also required and style
    
    * fix: remove comment
    
    * fix: topic name renaming
    
    ---------
    
    Co-authored-by: Haidong Xu <hai_designer@hotmail.com>
    Co-authored-by: marinovl7 <la4ezar71@abv.bg>

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,6 +67,7 @@ const config = {
       height: {
         '657px': '657px',
         '600px': '600px',
+        '0.5px': '0.5px',
       },
       padding: {
         '179px': '179px',
@@ -74,6 +75,9 @@ const config = {
       },
       borderRadius: {
         '12px_0_0_12px': '12px 0 0 12px',
+      },
+      borderSize: {
+        '0.5px': '0.5px',
       },
       zIndex: {
         9999: '9999',

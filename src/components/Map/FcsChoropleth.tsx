b03c697b27a9d0ec9e0c587883c00491c307a6d0
plaume8 <linus.sander@icloud.com>
2024-12-02 20:50:14 +0100

    fix: country name on hover - deactivated for FCS map if the country is selected

--- src/components/Map/FcsChoropleth.tsx
+++ src/components/Map/FcsChoropleth.tsx
@@ -1,6 +1,6 @@
 import L from 'leaflet';
 import { useTheme } from 'next-themes';
-import React, { useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { GeoJSON } from 'react-leaflet';
 
 import FcsChoroplethProps from '@/domain/props/FcsChoroplethProps';
@@ -8,6 +8,8 @@ import FcsChoroplethOperations from '@/operations/map/FcsChoroplethOperations';
 import { MapboxMapOperations } from '@/operations/map/MapboxMapOperations';
 
 import FscCountryChoropleth from './FcsCountryChoropleth';
+import { Feature } from 'geojson';
+import NutritionChoroplethOperations from '@/operations/map/NutritionChoroplethOperations.ts';
 
 export default function FcsChoropleth({
   data,
@@ -27,6 +29,22 @@ export default function FcsChoropleth({
     setSelectedCountryId(null);
   };
 
+  // adding the country name as a tooltip to each layer (on hover); the tooltip is not shown if the country is selected
+  useEffect(() => {
+    if (geoJsonRef.current) {
+      geoJsonRef.current.eachLayer((layer) => {
+        if (!('feature' in layer)) return;
+        const feature = layer.feature as Feature;
+        if (feature.properties?.adm0_id !== selectedCountryId) {
+          const tooltipContainer = MapboxMapOperations.createCountryNameTooltipElement(feature?.properties?.adm0_name);
+          layer.bindTooltip(tooltipContainer, { className: 'leaflet-tooltip', sticky: true });
+        } else {
+          layer.unbindTooltip();
+        }
+      });
+    }
+  }, [selectedCountryId]);
+
   return (
     <div>
       <GeoJSON
@@ -35,13 +53,9 @@ export default function FcsChoropleth({
         }}
         data={data}
         style={FcsChoroplethOperations.countryStyle}
-        onEachFeature={(feature, layer) => {
-          // tooltip on country hover -> showing name
-          const tooltipContainer = MapboxMapOperations.createCountryNameTooltipElement(feature?.properties?.adm0_name);
-          layer.bindTooltip(tooltipContainer, { className: 'leaflet-tooltip', sticky: true });
-
-          FcsChoroplethOperations.onEachFeature(feature, layer, setSelectedCountryId, theme === 'dark');
-        }}
+        onEachFeature={(feature, layer) =>
+          FcsChoroplethOperations.onEachFeature(feature, layer, setSelectedCountryId, theme === 'dark')
+        }
       />
       {regionData && countryId === selectedCountryId && (
         <FscCountryChoropleth

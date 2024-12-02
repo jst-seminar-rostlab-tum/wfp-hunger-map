35ba24acc9d143e41816fa5619df292f10640e03
georgi4444 <georgi.tsipov@gmail.com>
2024-12-02 19:40:18 +0100

    fix: calculate hunger alert positioning dynamically

--- src/components/HungerAlert/HungerAlertSkeleton.tsx
+++ src/components/HungerAlert/HungerAlertSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@nextui-org/skeleton';
 
 export default function HungerAlertSkeleton() {
   return (
-    <div className="absolute top-[360px] left-7 z-10 cursor-pointer ">
+    <div className="absolute top-[calc(50dvh+90px)] left-7 z-10 cursor-pointer ">
       <Skeleton
         className="rounded-full flex flex-col items-center justify-center text-center bg-white dark:bg-content2 relative p-5"
         style={{ width: '4rem', height: '4rem' }}

bf5bd08faa05040de313993fd1916b40e522ce1b
Bohdan Garchu <bohdan.garchu@gmail.com>
2024-12-07 18:28:53 +0100

    feat: use single selection component

---
+++ src/domain/props/CountrySelectionProps.ts
@@ -0,0 +1,9 @@
+import { GlobalFcsData } from '../entities/country/CountryFcsData';
+import { CountryMapData, CountryMapDataWrapper } from '../entities/country/CountryMapData';
+
+export interface CountrySelectionProps {
+  countryMapData: CountryMapDataWrapper;
+  globalFcsData: GlobalFcsData;
+  selectedCountries: CountryMapData[];
+  setSelectedCountries: (countries: CountryMapData[]) => void;
+}

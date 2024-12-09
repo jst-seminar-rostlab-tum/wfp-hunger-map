a47ea349532eacade1f348e44fc9c24f7a33ea04
Haidong Xu <haidong.xu@tum.de>
2024-12-09 18:20:00 +0100

    Feature/f 129 add error and successful toast to submit button back up (#113)
    * feat: init snackbar
    
    * feat: modify style
    
    * feat: improve snackbar style
    
    * fix: eslint issue
    
    * feat: remove submit btn status and move it into snackbar
    
    * feat: add snackbar animate
    
    * feat：improve snackbar logic
    
    * style: add snackbar doc
    
    * fix: pipeline
    
    * fix: temp delete from layout
    
    * feat: add layout back
    
    * fix: try to not use merge class
    
    * fix: remove import
    
    * fix: add back
    
    * fix: update yarn lock
    
    * fix: yarn lock
    
    * fix: change position and refactoring
    
    * fix: use message from backend
    
    * fix: change snackbar position
    
    ---------
    
    Co-authored-by: Haidong Xu <hai_designer@hotmail.com>

--- src/domain/entities/download/Country.ts
+++ src/domain/entities/download/Country.ts
@@ -39,3 +39,7 @@ export const COUNTRY_ERROR_MSG = 'Country is required';
 export const DATE_RANGE_ERROR_MSG = 'Date range is required';
 
 export const DATE_RANGE_TOO_LONG_ERROR_MSG = 'Date range must be less than 500 days';
+
+export const DOWNLOAD_SUCCESS_MSG = 'Download successful!';
+
+export const DOWNLOAD_ERROR_MSG = 'Download failed!';

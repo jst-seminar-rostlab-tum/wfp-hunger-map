ad32f75b62f17e43b73c5864e48f24fd4dc45f6f
Georgi Tsipov <76964999+georgi4444@users.noreply.github.com>
2024-12-10 22:38:50 +0100

    feat: add zoom control skeleton (#122)
    * feat: add zoom control skeleton
    
    * fix: copy Map.tsx structure for the skeleton

--- src/components/Map/MapSkeleton.tsx
+++ src/components/Map/MapSkeleton.tsx
@@ -1,5 +1,11 @@
+import ZoomControlSkeleton from '@/components/Map/ZoomControlSkeleton';
 import MapSkeletonData from '@/domain/constant/map/MapSkeletonData';
 
 export default function MapSkeleton() {
-  return <MapSkeletonData className="w-full h-full animate-opacityPulse fill-countriesBase bg-ocean" />;
+  return (
+    <div className="z-1 h-full w-full">
+      <MapSkeletonData className="h-full w-full animate-opacityPulse fill-countriesBase bg-ocean" />
+      <ZoomControlSkeleton />
+    </div>
+  );
 }

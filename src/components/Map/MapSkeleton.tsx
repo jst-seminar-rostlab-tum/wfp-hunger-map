80b6f2b99dfe9a00df964d821f604bb3cb3de45d
georgi4444 <georgi.tsipov@gmail.com>
2024-12-10 21:07:27 +0100

    fix: copy Map.tsx structure for the skeleton

--- src/components/Map/MapSkeleton.tsx
+++ src/components/Map/MapSkeleton.tsx
@@ -3,9 +3,9 @@ import MapSkeletonData from '@/domain/constant/map/MapSkeletonData';
 
 export default function MapSkeleton() {
   return (
-    <>
-      <MapSkeletonData className="w-full h-full animate-opacityPulse fill-countriesBase bg-ocean" />
+    <div className="z-1 h-full w-full">
+      <MapSkeletonData className="h-full w-full animate-opacityPulse fill-countriesBase bg-ocean" />
       <ZoomControlSkeleton />
-    </>
+    </div>
   );
 }
--- src/components/Map/ZoomControlSkeleton.tsx
+++ src/components/Map/ZoomControlSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '@nextui-org/skeleton';
 
 export default function ZoomControlSkeleton() {
   return (
-    <div className="absolute right-4 bottom-7 z-9999">
+    <div className="absolute right-4 bottom-7">
       <Skeleton className="bg-content1 dark:bg-content1 rounded-md w-10 h-16" />
     </div>
   );

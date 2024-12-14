ef631667ccd6160858a1cb8a2dc89ab3610a12f6
ahmetselman <ahmtslmngcl@gmail.com>
2024-12-14 11:45:30 +0100

    feat: log error

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -9,6 +9,7 @@ export default function Error({ error, reset }: { error: Error & { digest?: stri
   useEffect(() => {
     // Log the error to an error reporting service
     /* eslint-disable no-console */
+    console.error(error);
   }, [error]);
 
   return (

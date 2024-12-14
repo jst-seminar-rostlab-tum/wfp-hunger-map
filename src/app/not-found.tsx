653fc44b855db8ec2fb0aa95d774fe5b47dbe95b
ahmetselman <ahmtslmngcl@gmail.com>
2024-12-14 11:42:23 +0100

    feat: not-found 404

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { CustomButton } from '@/components/Buttons/CustomButton';
+import { Topbar } from '@/components/Topbar/Topbar';
+
+export default function NotFound() {
+  return (
+    <div className="flex flex-col h-screen">
+      <Topbar />
+      <main className="flex h-full flex-col items-center justify-center gap-2 text-content">
+        <section className="text-center">
+          <h2 className="text-xl font-semibold">404 Not Found</h2>
+          <p>Could not find the requested resource.</p>
+        </section>
+        <div className="flex flex-row gap-2 mt-4">
+          <a href="/">
+            <CustomButton variant="solid" size="lg">
+              Go to Home page
+            </CustomButton>
+          </a>
+        </div>
+      </main>
+    </div>
+  );
+}

c328e522c96e3c1a9e72b7edce94516a61d145a5
Haidong Xu <hai_designer@hotmail.com>
2024-12-15 21:41:16 +0100

    feat: add mock robots

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://localhost:3000', // local temporary
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/admin', '/api'], 
+};

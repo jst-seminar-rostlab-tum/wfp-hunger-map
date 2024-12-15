fb6bd51250db1680f269bb52928ee459e9d07c13
Ahmet Selman Güclü <77407148+ahmtslmngcl@users.noreply.github.com>
2024-12-15 14:57:33 +0100

    feat: configure error page (#132)
    * feat: configure error page
    
    * feat: not-found 404
    
    * feat: log error
    
    * Update not-found.tsx
    
    ---------
    
    Co-authored-by: Lachezar Marinov <99186919+marinovl7@users.noreply.github.com>

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,29 +1,18 @@
 'use client';
 
-import { useEffect } from 'react';
-
 import { CustomButton } from '@/components/Buttons/CustomButton';
 import { Topbar } from '@/components/Topbar/Topbar';
 
-export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
-  useEffect(() => {
-    // Log the error to an error reporting service
-    /* eslint-disable no-console */
-    console.error(error);
-  }, [error]);
-
+export default function NotFound() {
   return (
     <div className="flex flex-col h-screen">
       <Topbar />
       <main className="flex h-full flex-col items-center justify-center gap-2 text-content">
         <section className="text-center">
-          <h2 className="text-xl font-semibold">An Error Occurred</h2>
-          <p>Please try again or go back to the Home page.</p>
+          <h2 className="text-xl font-semibold">Ooops</h2>
+          <p>The requested page could not be found.</p>
         </section>
         <div className="flex flex-row gap-2 mt-4">
-          <CustomButton variant="solid" size="lg" onClick={() => reset()}>
-            Try again
-          </CustomButton>
           <a href="/">
             <CustomButton variant="solid" size="lg">
               Go to Home page

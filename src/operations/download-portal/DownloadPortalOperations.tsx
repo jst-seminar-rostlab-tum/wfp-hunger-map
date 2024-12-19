c8f62b6e9511ecf0b49ddfe8c8899470f8276f7d
Bohdan Garchu <bohdan.garchu@gmail.com>
2024-12-19 21:56:54 +0100

    feat: year in review reports

--- src/operations/download-portal/DownloadPortalOperations.tsx
+++ src/operations/download-portal/DownloadPortalOperations.tsx
@@ -4,7 +4,11 @@ import { Bot } from 'lucide-react';
 
 import { CountryCodesData } from '@/domain/entities/country/CountryCodesData';
 import { ICountryData } from '@/domain/entities/download/Country';
+import { SNACKBAR_SHORT_DURATION } from '@/domain/entities/snackbar/Snackbar';
+import { YearInReview } from '@/domain/entities/YearInReview';
+import { SnackbarPosition, SnackbarStatus } from '@/domain/enums/Snackbar';
 import { CustomTableColumns } from '@/domain/props/CustomTableProps';
+import { SnackbarProps } from '@/domain/props/SnackbarProps';
 
 export class DownloadPortalOperations {
   static getColumns(): CustomTableColumns {
@@ -16,13 +20,14 @@ export class DownloadPortalOperations {
     ] as CustomTableColumns;
   }
 
-  static formatTableData(
+  static formatCountryTableData(
     data: CountryCodesData[],
     setSelectedCountry: (countryData: CountryCodesData) => void,
     initiateChatAboutReport: (country: string, report: string) => Promise<void>,
     setPdfFile: (file: Blob | null) => void,
     setError: (error: string | null) => void,
-    toggleModal: () => void
+    toggleModal: () => void,
+    showSnackBar: (snackbarProps: SnackbarProps) => void
   ) {
     return data.map((item) => ({
       keyColumn: item.country.name,
@@ -41,7 +46,7 @@ export class DownloadPortalOperations {
         <div className="flex justify-center items-center">
           <DocumentDownload
             size={20}
-            onClick={() => DownloadPortalOperations.downloadPdf(item)}
+            onClick={() => DownloadPortalOperations.downloadCountryReport(item, showSnackBar)}
             className="cursor-pointer"
           />
         </div>
@@ -51,6 +56,53 @@ export class DownloadPortalOperations {
           <Bot
             size={20}
             onClick={() => initiateChatAboutReport(item.country.name, item.url.summary)}
+            className="cursor-pointer"
+          />
+        </div>
+      ),
+    }));
+  }
+
+  static formatYearInReviewTableData(
+    data: YearInReview[],
+    setSelectedReport: (report: YearInReview) => void,
+    initiateChatAboutReport: (country: string, report: string) => Promise<void>,
+    setPdfFile: (file: Blob | null) => void,
+    setError: (error: string | null) => void,
+    toggleModal: () => void,
+    showSnackBar: (snackbarProps: SnackbarProps) => void
+  ) {
+    return data.map((item) => ({
+      keyColumn: item.label,
+      preview: (
+        <div className="flex justify-center items-center">
+          <SearchNormal1
+            size={20}
+            onClick={() => {
+              setSelectedReport(item);
+              DownloadPortalOperations.handlePreview(item.url, setPdfFile, setError);
+              toggleModal();
+            }}
+            className="cursor-pointer"
+          />
+        </div>
+      ),
+      download: (
+        <div className="flex justify-center items-center">
+          <DocumentDownload
+            size={20}
+            onClick={() => DownloadPortalOperations.downloadYearInReview(item, showSnackBar)}
+            className="cursor-pointer"
+          />
+        </div>
+      ),
+      chat: (
+        <div className="flex justify-center items-center">
+          <Bot
+            size={20}
+            onClick={() => {
+              initiateChatAboutReport(item.label, item.url);
+            }}
             className="cursor-pointer"
           />
         </div>
@@ -77,13 +129,51 @@ export class DownloadPortalOperations {
     return Math.ceil(diffTime / (1000 * 60 * 60 * 24));
   }
 
-  static downloadPdf(country: CountryCodesData) {
-    const link = document.createElement('a');
-    link.href = country.url.summary;
-    link.download = `${country.country.name}.pdf`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  static async downloadCountryReport(
+    country: CountryCodesData,
+    showSnackBar: (snackbarProps: SnackbarProps) => void
+  ): Promise<void> {
+    await this.downloadFile(country.url.summary, `${country.country.name}.pdf`, showSnackBar);
+  }
+
+  static async downloadYearInReview(
+    yearInReview: YearInReview,
+    showSnackBar: (snackbarProps: SnackbarProps) => void
+  ): Promise<void> {
+    await this.downloadFile(yearInReview.url, `${yearInReview.label}.pdf`, showSnackBar);
+  }
+
+  private static async downloadFile(
+    fileUrl: string,
+    fileName: string,
+    showSnackBar?: (snackbarProps: SnackbarProps) => void
+  ): Promise<void> {
+    try {
+      const response = await fetch(fileUrl);
+      if (!response.ok) throw new Error('Failed to download file');
+
+      const blob = await response.blob();
+      const url = window.URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = fileName;
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+
+      // Cleanup URL object
+      window.URL.revokeObjectURL(url);
+    } catch {
+      if (showSnackBar) {
+        showSnackBar({
+          message: 'Error downloading file',
+          status: SnackbarStatus.Error,
+          position: SnackbarPosition.BottomRight,
+          duration: SNACKBAR_SHORT_DURATION,
+        });
+      }
+    }
   }
 
   static async fetchPdfAsByteStream(url: string): Promise<ArrayBuffer> {

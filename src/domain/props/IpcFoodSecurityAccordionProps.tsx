5033db4b4ddf3e9604255fbacd5d0c3757d62c06
Armanpreet Ghotra <arman.ghotra@tum.de>
2024-12-19 13:42:17 +0100

    fix: remove unwanted props file

--- src/domain/props/IpcFoodSecurityAccordionProps.tsx
+++
@@ -1,7 +0,0 @@
-import { CountryData } from '../entities/country/CountryData';
-
-export default interface IpcFoodSecurityAccordionProps {
-  countryData: CountryData | undefined;
-  deltaOneMonth: number | null;
-  deltaThreeMonth: number | null;
-}
--- src/domain/props/IpcNutritionAccordionProps.tsx
+++
@@ -1,5 +0,0 @@
-import { CountryIso3Data } from '../entities/country/CountryIso3Data';
-
-export default interface IpcFoodSecurityAccordionProps {
-  countryIso3Data: CountryIso3Data | undefined;
-}

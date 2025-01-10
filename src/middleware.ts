782611d4e77b9fe1d85e944946bd5f5678f138ea
Sámuel Fekete <feketesamu@gmail.com>
2025-01-10 22:11:55 +0100

    fix: update csp

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'sha256-q1+DaXsZUnEJs3jpN9ZoWp6ypK1xBwXiRxG+C31xOUA=' 'strict-dynamic' 'unsafe-eval' 'unsafe-inline' ${process.env.NEXT_PUBLIC_API_URL?.slice(0, -3)};
     style-src 'self' 'unsafe-inline';
-    connect-src 'self' https://region1.google-analytics.com https://cdn.hungermapdata.org ${process.env.NEXT_PUBLIC_API_URL?.slice(0, -3)} ${process.env.NEXT_PUBLIC_V3_API_URL?.slice(0, -3)} ${process.env.NEXT_PUBLIC_CHATBOT_API_URL ?? ''};
+    connect-src 'self' https://region1.google-analytics.com https://static.hungermapdata.org https://cdn.hungermapdata.org ${process.env.NEXT_PUBLIC_API_URL?.slice(0, -3)} ${process.env.NEXT_PUBLIC_V3_API_URL?.slice(0, -3)} ${process.env.NEXT_PUBLIC_CHATBOT_API_URL ?? ''};
     img-src 'self' blob: data: https://static.hungermapdata.org https://dev.api.earthobservation.vam.wfp.org;
     font-src 'self';
     object-src 'none';

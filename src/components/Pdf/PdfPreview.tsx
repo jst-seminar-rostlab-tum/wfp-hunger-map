c3c4a6ef21b6424eb5d2e8c8ddabe97d01784662
Bohdan Garchu <bohdan.garchu@gmail.com>
2025-01-11 20:58:40 +0300

    feat: pdf preview inline docs

--- src/components/Pdf/PdfPreview.tsx
+++ src/components/Pdf/PdfPreview.tsx
@@ -4,7 +4,25 @@ import PdfPreviewProps from '@/domain/props/PdfPreviewProps';
 
 import PdfLoader from './PdfLoader';
 
-export default function PdfPreview({ isModalOpen, toggleModal, pdfFile, error, onDownloadPdf }: PdfPreviewProps) {
+/**
+ * The `PdfPreview` component renders a modal to preview a PDF file.
+ * Includes functionality to toggle the modal, display errors, and download the PDF file.
+ *
+ * @param {Object} props The properties for the PdfPreview component.
+ * @param {boolean} props.isModalOpen Indicates whether the modal is open.
+ * @param {() => void} props.toggleModal Function to toggle the modal's open state.
+ * @param {string | Blob | null} props.pdfFile The PDF file to be displayed in the modal.
+ * @param {string | null} [props.error] An error message to display if the PDF cannot be loaded.
+ * @param {() => void} [props.onDownloadPdf] Function to handle downloading the PDF file.
+ * @returns {JSX.Element} The rendered PdfPreview component.
+ */
+export default function PdfPreview({
+  isModalOpen,
+  toggleModal,
+  pdfFile,
+  error,
+  onDownloadPdf,
+}: PdfPreviewProps): JSX.Element {
   return (
     <Modal
       backdrop="opaque"

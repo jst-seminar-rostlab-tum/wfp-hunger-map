f813a364780f50854efc48fe807ee7081adc8b64
Jakob Schödl <jakob.schoedl@tum.de>
2025-01-18 10:38:52 +0100

    docs: RegionFcs.ts

--- src/domain/entities/region/RegionFcs.ts
+++ src/domain/entities/region/RegionFcs.ts
@@ -2,14 +2,20 @@
  * This is displayed on the tooltip when you hover on a region
  */
 export interface RegionFcs {
+  // FCS Score = share of people with insufficient food consumption (0-1)
   score: number;
   scoreLow: number;
   scoreHigh: number;
+
+  // Percentage of people with insufficient food consumption (0-100, 2 decimals)
   ratio: number;
   ratioLow: number;
   ratioHigh: number;
+
+  // Number of people with insufficient food consumption (in Millions)
   people: number;
   peopleLow: number;
   peopleHigh: number;
+
   dataType: string;
 }
--- src/domain/entities/region/RegionRcs.ts
+++ src/domain/entities/region/RegionRcs.ts
@@ -1,7 +1,10 @@
 export interface RegionRcsi {
+  // Percentage of people crisis or above crisis food-based coping (0-100, 2 decimals)
   ratio: number;
   ratioLow: number;
   ratioHigh: number;
+
+  // Number of people with crisis or above crisis food-based coping (in Millions)
   people: number;
   peopleLow: number;
   peopleHigh: number;

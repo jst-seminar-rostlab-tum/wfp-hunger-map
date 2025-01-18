8f474689b3dfd2e6acdede8dbaade6765ebb7d70
Sámuel Fekete <feketesamu@gmail.com>
2025-01-18 18:44:50 +0100

    feat: refactor region labels

--- src/components/Map/NutritionMap/NutritionStateChoropleth.tsx
+++ src/components/Map/NutritionMap/NutritionStateChoropleth.tsx
@@ -16,7 +16,6 @@ import CountryLoadingLayer from '../CountryLoading';
  * renders the Nutrition Map for country view.
  * @param {NutritionStateChoroplethProps} props - The props of the component.
  * @param {FeatureCollection} props.countryMapData - The GeoJSON data of the region.
- * @param {(tooltips: (prevRegionLabelData: L.Tooltip[]) => L.Tooltip[]) => void} props.setRegionLabelTooltips - Function to set the region label tooltips.
  * @param {NutrientType} props.selectedNutrient - The selected nutrient.
  * @param {() => void} [props.onDataUnavailable] - A callback to signal to the parent component that there's no regional Nutrition data for this country
 
@@ -24,7 +23,6 @@ import CountryLoadingLayer from '../CountryLoading';
  */
 
 export default function NutritionStateChoropleth({
-  setRegionLabelTooltips,
   countryMapData,
   onDataUnavailable,
   selectedNutrient,
@@ -65,17 +63,21 @@ export default function NutritionStateChoropleth({
   }, [nutritionData]);
 
   useEffect(() => {
+    let tooltips: L.Tooltip[] = [];
     if (!hasRendered.current) {
       // Skip the effect on the initial render
       hasRendered.current = true;
-      return;
+      return () => {};
     }
     if (regionLabelData && nutritionData) {
-      const tooltips = nutritionData.features.map((f) =>
-        MapOperations.setupRegionLabelTooltip(f, regionLabelData, countryData.iso3, map)
-      );
-      setRegionLabelTooltips(tooltips.filter((t): t is L.Tooltip => !!t));
+      tooltips = nutritionData.features
+        .map((f) => MapOperations.setupRegionLabelTooltip(f, regionLabelData, countryData.iso3, map))
+        .filter((t): t is L.Tooltip => !!t);
     }
+
+    return () => {
+      tooltips.forEach((tooltip) => tooltip.removeFrom(map));
+    };
   }, [dataLoaded]);
 
   return isLoading || !nutritionData ? (

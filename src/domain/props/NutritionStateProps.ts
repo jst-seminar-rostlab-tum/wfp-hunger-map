cfa348a641225e9012632b2c15a74e4316d5247a
Sámuel Fekete <feketesamu@gmail.com>
2025-01-18 18:50:46 +0100

    fix: build

--- src/domain/props/NutritionStateProps.ts
+++ src/domain/props/NutritionStateProps.ts
@@ -1,12 +1,10 @@
 import { FeatureCollection, Geometry } from 'geojson';
-import L from 'leaflet';
 
 import { CountryProps } from '@/domain/entities/country/CountryMapData.ts';
 
 import { NutrientType } from '../enums/NutrientType';
 
 export interface NutritionStateChoroplethProps {
-  setRegionLabelTooltips: (tooltips: L.Tooltip[]) => void;
   countryMapData: FeatureCollection<Geometry, CountryProps>;
   onDataUnavailable: () => void;
   selectedNutrient: NutrientType;

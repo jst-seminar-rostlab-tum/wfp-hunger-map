1b806a437dcaeb28db12c80c491fbef5b0cf912c
Armanpreet Ghotra <arman.ghotra@tum.de>
2025-01-25 22:40:14 +0100

    fix: add margin bottom as props

--- src/components/Map/FcsMap/FcsAccordion.tsx
+++ src/components/Map/FcsMap/FcsAccordion.tsx
@@ -78,6 +78,7 @@ export default function FcsAccordion({ countryName, countryId, countryCode }: Fc
           maxWidth={600}
           items={foodSecurityAccordionItems}
           expandAll={!countryDataLoading && !iso3DataLoading && !countryForecastDataLoading}
+          noPadding
         />
       </div>
       <div className="absolute w-[350px] right-[1rem] inset-y-24 z-9999">

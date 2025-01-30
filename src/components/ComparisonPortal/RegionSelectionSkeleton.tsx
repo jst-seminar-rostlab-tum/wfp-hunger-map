6adb52c6d68ba86533702f080842340afd7f4787
Bohdan Garchu <107936554+bohdangarchu@users.noreply.github.com>
2025-01-30 19:54:37 +0100

    fix: selection skeleton (#232)

---
+++ src/components/ComparisonPortal/RegionSelectionSkeleton.tsx
@@ -0,0 +1,19 @@
+import { Skeleton } from '@nextui-org/skeleton';
+
+/**
+ * A skeleton for the Select component.
+ * @returns {JSX.Element}
+ */
+export default function SelectionSkeleton(): JSX.Element {
+  return (
+    <div className="flex-1 basis-0 min-w-48">
+      <div className="group flex flex-col w-full">
+        <div className="w-full flex flex-col">
+          <Skeleton aria-hidden="true" className="relative px-3 gap-3 w-full shadow-sm h-10 min-h-10 rounded-medium">
+            <div className="inline-flex h-full w-[calc(100%_-_theme(spacing.6))] min-h-4 items-center gap-1.5 box-border" />
+          </Skeleton>
+        </div>
+      </div>
+    </div>
+  );
+}
